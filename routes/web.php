d8ca83ea346304f2c2c05c8680d6b6d00336fd33
chavandeepak182 <chavandeepak182@gmail.com>
2025-09-05 15:34:34 +0530

    blog updated as blogs

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::get('/privacy-policy', function () {
 
 
 // Single blog details page
-Route::get('/blog/{id}', [FrontendController::class, 'showBlog'])->name('blog.show');
+Route::get('/blogs/{id}', [FrontendController::class, 'showBlog'])->name('blog.show');
 
 Route::get('/blogs', [FrontendController::class, 'blog'])->name('blog');
 
